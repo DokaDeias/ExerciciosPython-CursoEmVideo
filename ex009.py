530d773e919c7dedbe62fd6c4000bb8e9be800a8
Matheus Anjos <mssanjoss@gmail.com>
2026-07-20 18:51:01 -0300

    Add files via upload

--- ex009.py
+++ ex009.py
@@ -8,20 +8,18 @@ c = n * 5
 s = n * 6
 st = n * 7
 o = n * 8
-n = n * 9
+no = n * 9
 dez = n * 10
 
-print('-------------------',
-      n, 'x 1 = ', u,
-      ''
-      ''
-      ''
-      ''
-      ''
-      ''
-      ''
-      ''
-      ''
-      ''
-      ''
-      '-------------------')
+print('-' * 12)
+print(n, 'x 1=',u)
+print(n, 'x 2=',d)
+print(n, 'x 3=',t)
+print(n, 'x 4=',q)
+print(n, 'x 5=',c)
+print(n, 'x 6=',s)
+print(n, 'x 7=',st)
+print(n, 'x 8=',o)
+print(n, 'x 9=',no)
+print(n, 'x 10=',dez)
+print('-' * 12)
